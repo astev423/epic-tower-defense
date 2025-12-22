2d0d10bc5285c17a15c3a4bf8ce72a591b28582d
astev423 <alex.stvn2005@gmail.com>
2025-12-22 13:12:19 -0800

    refactor: scaling tilemap was messed up, changed camera to 1920x1080p so tiles don't change scale

--- ui/tower_selection/tower_selection.gd
+++ ui/tower_selection/tower_selection.gd
@@ -36,46 +36,27 @@ func _process(delta: float) -> void:
 
 
 func attempt_placing_tower_on_grid() -> void:
+	# Can't place on the tower selection UI
+	if get_global_mouse_position().x > 2112:
+		return
 	# This converts the local pos of the mouse to the tile number using local_to_map
 	var cell_position := tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
 	# Get data associated with that cell, see if it has placeable marked as true in tileset
 	var is_placeable = tile_map_layer.get_cell_tile_data(cell_position).get_custom_data("placeable")
 	if is_placeable and !used_tiles.has(cell_position):
 		var new_tower = CANNON_SCENE.instantiate()
-		add_child(new_tower)
+		get_parent().add_child(new_tower)
 		new_tower.global_position = cell_position * 64
-		mark_used_tiles(cell_position, new_tower)
+		used_tiles[cell_position] = new_tower
 
 
+## Free tower if it exists and mark tiles it occupied as placeable again
 func attempt_delete_tower_on_grid() -> void:
 	var cell_position := tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
-	var is_placeable = tile_map_layer.get_cell_tile_data(cell_position).get_custom_data("placeable")
-	if is_placeable and used_tiles.has(cell_position):
+	if used_tiles.has(cell_position):
 		var tower = used_tiles[cell_position]
-		erase_all_keys_with_value(tower)
+		used_tiles.erase(cell_position)
 		tower.queue_free()
-
-
-## Can't use any tiles near tower
-func mark_used_tiles(cell_position: Vector2i, new_tower) -> void:
-	var x = cell_position.x
-	var y = cell_position.y
-	used_tiles[cell_position] = new_tower
-	used_tiles[Vector2i(x + 1, y)] = new_tower
-	used_tiles[Vector2i(x + 1, y + 1)] = new_tower
-	used_tiles[Vector2i(x - 1, y)] = new_tower
-	used_tiles[Vector2i(x - 1, y + 1)] = new_tower
-	used_tiles[Vector2i(x, y + 1)] = new_tower
-	used_tiles[Vector2i(x, y - 1)] = new_tower
-
-
-func erase_all_keys_with_value(value_to_match) -> void:
-	var keys_to_erase: Array = []
-	for k in used_tiles.keys():
-		if used_tiles[k] == value_to_match:
-			keys_to_erase.append(k)
-	for k in keys_to_erase:
-		used_tiles.erase(k)
 
 
 func make_tower_follow_mouse() -> void:
@@ -90,7 +71,7 @@ func on_select_cannon_pressed() -> void:
 		held_tower_instance.queue_free()
 
 	held_tower_instance = CANNON_SCENE.instantiate()
-	self.add_child(held_tower_instance)
+	get_parent().add_child(held_tower_instance)
 
 
 func deselect_held_tower() -> void:
